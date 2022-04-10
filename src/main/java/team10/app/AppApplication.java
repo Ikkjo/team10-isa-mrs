a033934ce8ed14a8727233d31566ffcde9d5cba6
ikkjo <ilija.kalinic@gmail.com>
2022-04-10 19:06:51 +0200

    Initial commit

---
+++ src/main/java/team10/app/AppApplication.java
@@ -0,0 +1,13 @@
+package team10.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
