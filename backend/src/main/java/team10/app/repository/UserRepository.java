6377bc816fdcc99577efae4bd44dafd77b04418f
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-23 22:36:07 +0200

    Added AllArgsConstructor to UserRepository

--- backend/src/main/java/team10/app/repository/UserRepository.java
+++ backend/src/main/java/team10/app/repository/UserRepository.java
@@ -1,14 +1,26 @@
 package team10.app.repository;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import team10.app.model.User;
+import team10.app.model.VacationHomeOwner;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+@AllArgsConstructor
+public class UserRepository {
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
+    // TODO: Add other repos
+
+    private final VacationHomeOwnerRepository vacationHomeOwnerRepository;
+    private final ShipOwnerRepository shipOwnerRepository;
+
+    public boolean userExists(String email) {
+        return vacationHomeOwnerRepository.findByEmail(email).isPresent()
+                || shipOwnerRepository.findByEmail(email).isPresent();
+    }
 
 }
