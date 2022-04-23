57d6f907ddba8249bba61f7e26c63f486f649eb5
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-23 19:43:48 +0200

    Adds RegistrationRequest

---
+++ backend/src/main/java/team10/app/dto/RegistrationRequest.java
@@ -0,0 +1,16 @@
+package team10.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import team10.app.model.BusinessPartner;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final BusinessPartner user;
+    private final String description;
+}
