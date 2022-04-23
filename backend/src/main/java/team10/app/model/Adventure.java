69e045f307b2d2cd512714307213b33b10616012
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-23 22:36:49 +0200

    Fixed Adventure

--- backend/src/main/java/team10/app/model/Adventure.java
+++ backend/src/main/java/team10/app/model/Adventure.java
@@ -1,17 +1,26 @@
 package team10.app.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Adventure extends RentalEntity {
     private String instructorBiography;
+    @ElementCollection
+    @CollectionTable(name = "my_fishing", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "fishing", nullable = false)
     private List<String> fishing;
     private String cancellationConditions;
     private int maxCapacity;
+    @Id
+    private Long id;
 
     public Adventure(String name,
                      String description,
@@ -32,5 +41,13 @@ public class Adventure extends RentalEntity {
         this.maxCapacity = maxCapacity;
         this.fishing = fishing;
         this.cancellationConditions = cancellationConditions;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
