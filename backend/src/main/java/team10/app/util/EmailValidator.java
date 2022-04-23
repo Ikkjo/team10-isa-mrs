7ad091759e9a3b8990f80891c0c1a13ab0bfd7ba
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-23 22:28:42 +0200

    Fixed email validator regex

--- backend/src/main/java/team10/app/util/EmailValidator.java
+++ backend/src/main/java/team10/app/util/EmailValidator.java
@@ -10,6 +10,6 @@ public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
         String regexPattern = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"; // Email regex of RFC5322 Standard: https://www.rfc-editor.org/info/rfc5322
-        return regexPattern.matches(s);
+        return !regexPattern.matches(s);
     }
 }
