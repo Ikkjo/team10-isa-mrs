05b3365396389f0adadff125236e88976c7f1ab6
Nikola Damjanovic <cipinikolica@gmail.com>
2022-04-23 21:13:05 +0200

    Implement FishingInstructorRepositoriy

--- backend/src/main/java/team10/app/repository/FishingInstructorRepository.java
+++ backend/src/main/java/team10/app/repository/FishingInstructorRepository.java
@@ -1,4 +1,23 @@
 package team10.app.repository;
 
-public interface FishingInstructorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import team10.app.model.FishingInstructor;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true) // provjeriti da li treba
+public interface FishingInstructorRepository extends JpaRepository<FishingInstructor, Long> {
+
+        Optional<FishingInstructor> findByEmail(String email);
+
+        @Transactional
+        @Modifying
+        @Query("UPDATE User u SET u.enabled = TRUE WHERE u.email = ?1")
+        int enableFishingInstructor(String email);
+
 }
