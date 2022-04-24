18ec50531b0895e9473010cd954a1f37c1bb1e83
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-24 19:35:05 +0200

    Fixes cors config

--- backend/src/main/java/team10/app/security/config/WebSecurityConfig.java
+++ backend/src/main/java/team10/app/security/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package team10.app.security.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,11 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @AllArgsConstructor
@@ -27,6 +33,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .authenticated().and()
                 .cors().and()
                 .formLogin();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        // https://docs.spring.io/spring-security/site/docs/4.2.x/reference/html/cors.html
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
