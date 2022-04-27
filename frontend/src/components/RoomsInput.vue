4fda74d957f36098abd8f9e2e3a57bb0b4095037
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-27 19:48:42 +0200

    Fixes room input logic

--- frontend/src/components/RoomsInput.vue
+++ frontend/src/components/RoomsInput.vue
@@ -2,17 +2,17 @@
     <div id="rooms-input">
         <div class="form-control">
             <label for="num-rooms">Rooms</label>
-            <input  v-model="numRooms" type="number" name="num-rooms">   
+            <input v-model="numRooms" @change="addRoom" type="number" name="num-rooms">   
         </div>
         <div class="form-control">
             <label for="room">Room</label>
-            <select v-model="selectedRoom" @change="onChange($event)" name="room" id="room">
+            <select v-model="selectedRoom" @change="onSelectedRoomChange" name="room" id="room">
                 <option v-for="i in Number(numRooms)" :key="i" :value="'Room '+ i" @select="selectedRoom = i">Room {{i}}</option>
             </select>
         </div>
         <div class="form-control">
             <label for="beds">Beds</label>
-            <input type="number" name="beds" id="beds" :placeholder="bedPlaceholder" :value="bedInputValue">
+            <input @change="onBedChange" type="number" name="beds" id="beds" :placeholder="bedPlaceholder" :value="bedInputValue">
         </div>
             
             
@@ -31,9 +31,32 @@ export default {
         }
     },
     methods: {
-        onChange(event) {
+        addRoom(event) {
+             console.log(this.rooms.length);
+            if (event.target.value > this.rooms.length)
+                this.rooms.push({name: 'Room '+ (this.rooms.length+1), beds: 0});
+            else if (event.target.value < this.rooms.length)
+                if (this.rooms.length > 0) this.rooms.pop()
+           
+        },
+        onSelectedRoomChange(event) {
             if (this.selectedRoom != event.target.value) this.bedInputValue = 0
+            for (let room of this.rooms) {
+                if (room.name === this.selectedRoom) {
+                    this.bedInputValue = room.beds;
+                }
+            }
+        },
+        onBedChange(event) {
+            for (let room of this.rooms) {
+                if (room.name === this.selectedRoom) {
+                    room.beds = event.target.value.trim();
+                    break;
+                }
+            }
+                
         }
+
         
     },
     computed: {
