1f401b7ea9f507939b332bf83af23c943fae3f00
Vladan Mikic <vladanmikic2000@gmail.com>
2022-04-27 22:40:04 +0200

    Extracts address input to a component

---
+++ frontend/src/components/AddressInput.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="wrapper address-wrapper">
+            <div class="form-control">
+                <label for="address">Address</label>
+                <input type="text" :value="address" @keyup="$emit('update:address', address);" name="address" placeholder="">
+            </div>
+            <div class="form-control">
+                <label for="city">City</label>
+                <input type="text" :value="city" @keyup="$emit('update:city', city);" name="city" placeholder="">
+            </div>
+             <div class="form-control">
+                <label for="country" class="block-label">Country</label>
+                <country-select value:="country" @keyup="$emit('update:country', country);" :country="country" topCountry="US"/>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'AddressInput',
+    props: {
+        address: String,
+        city: String,
+        country: String,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
