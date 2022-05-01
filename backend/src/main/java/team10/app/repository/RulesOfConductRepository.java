6bbd2e2be671c6d52ffe2d8204e0920314aeb678
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-01 13:24:27 +0200

    Removes RulesOfConduct class and repo

--- backend/src/main/java/team10/app/repository/RulesOfConductRepository.java
+++
@@ -1,7 +0,0 @@
-package team10.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team10.app.model.RulesOfConduct;
-
-public interface RulesOfConductRepository extends JpaRepository<RulesOfConduct, Long> {
-}
