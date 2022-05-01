a667affa91eba70becfb777dcace1306b983873a
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-01 23:15:26 +0200

    Adds Validator to VacationHomeOwnerService, addVacationHome

--- backend/src/main/java/team10/app/service/VacationHomeOwnerService.java
+++ backend/src/main/java/team10/app/service/VacationHomeOwnerService.java
@@ -1,10 +1,12 @@
 package team10.app.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team10.app.dto.VacationHomeDTO;
 import team10.app.model.VacationHome;
 import team10.app.repository.*;
+import team10.app.util.Validator;
 
 @Service
 @AllArgsConstructor
@@ -13,8 +15,10 @@ public class VacationHomeOwnerService {
     private final VacationHomeOwnerRepository vacationHomeOwnerRepository;
     private final VacationHomeRepository vacationHomeRepository;
     private final AddressRepository addressRepository;
+    private final Validator validator;
 
     public String addVacationHome(VacationHomeDTO request) {
+        if (!validator.validateVacationHomeDTO(request)) {}// return bad request
         VacationHome vacationHome = new VacationHome(request);
         saveVacationHome(vacationHome);
         return "succeess";
