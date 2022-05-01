b1daf6e2c62454964f5de9463a21c9d2c857a62a
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-01 23:15:52 +0200

    Adds findByAddress in VacationHomeRepository

--- backend/src/main/java/team10/app/repository/VacationHomeRepository.java
+++ backend/src/main/java/team10/app/repository/VacationHomeRepository.java
@@ -1,12 +1,18 @@
 package team10.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import team10.app.model.Address;
 import team10.app.model.VacationHome;
+import team10.app.model.VacationHomeOwner;
 
 import java.util.Optional;
 
 @Repository
 public interface VacationHomeRepository extends JpaRepository<VacationHome, Long> {
+
+    @Query("select vH from VacationHome vH where vH.address = ?1")
+    Optional<VacationHome> findByAddress(Address address);
 
 }
