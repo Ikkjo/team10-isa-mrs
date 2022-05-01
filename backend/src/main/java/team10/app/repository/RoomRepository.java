ae452e86b195d4bc36f6f9792cdde459ae720ea7
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-01 13:24:32 +0200

    Removes Room class and repo

--- backend/src/main/java/team10/app/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package team10.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team10.app.model.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
