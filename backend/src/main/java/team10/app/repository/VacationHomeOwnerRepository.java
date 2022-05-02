36fecbc6d42976697ecbe56a846cca6456da5757
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-02 17:09:23 +0200

    Removes readonly from VacationHomeOwnerRepository

--- backend/src/main/java/team10/app/repository/VacationHomeOwnerRepository.java
+++ backend/src/main/java/team10/app/repository/VacationHomeOwnerRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import team10.app.model.User;
 import team10.app.model.VacationHomeOwner;
 
 import java.util.Optional;
 
 @Repository
-@Transactional(readOnly = true) // provjeriti da li treba
+@Transactional
 public interface VacationHomeOwnerRepository extends JpaRepository<VacationHomeOwner, Long> {
 
     Optional<VacationHomeOwner> findByEmail(String email);
