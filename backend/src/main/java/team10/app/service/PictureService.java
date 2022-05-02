a2b02b2cd28e8db45ad8a97d954ae7d4339f8bf3
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-02 19:27:22 +0200

    Adds PictureRepository and PictureService

---
+++ backend/src/main/java/team10/app/service/PictureService.java
@@ -0,0 +1,70 @@
+package team10.app.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import team10.app.model.Picture;
+import team10.app.repository.PictureRepository;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+
+@Service
+@AllArgsConstructor
+public class PictureService {
+
+    private final PictureRepository pictureRepository;
+
+    public Set<Picture> buildPictureSet(List<MultipartFile> pictures) throws IOException {
+        Set<Picture> pictureSet = new HashSet<>();
+        for (MultipartFile file : pictures) {
+            pictureSet.add(new Picture(file.getOriginalFilename(), file.getContentType(),
+                    compressBytes(file.getBytes())));
+        }
+        return pictureSet;
+    }
+
+    public static byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+
+        return outputStream.toByteArray();
+    }
+
+    // uncompress the image bytes before returning it to the angular application
+    public static byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException ioe) {
+        } catch (DataFormatException e) {
+        }
+        return outputStream.toByteArray();
+    }
+}
