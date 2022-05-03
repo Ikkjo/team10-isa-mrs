b60f8e75b24caabd37a0a0616becd02ac47d9bc0
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-03 20:07:44 +0200

    Adds return to addVacationHome

--- backend/src/main/java/team10/app/controller/VacationHomeOwnerController.java
+++ backend/src/main/java/team10/app/controller/VacationHomeOwnerController.java
@@ -1,6 +1,8 @@
 package team10.app.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team10.app.dto.VacationHomeDto;
 import team10.app.service.VacationHomeOwnerService;
@@ -14,8 +16,14 @@ public class VacationHomeOwnerController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/vacation-homes")
-    public String addVacationHome(@RequestBody VacationHomeDto request) {
-        return vacationHomeOwnerService.addVacationHome(request);
+    public ResponseEntity<VacationHomeDto> addVacationHome(@RequestBody VacationHomeDto request) {
+        try {
+             Boolean success = vacationHomeOwnerService.addVacationHome(request);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(request, HttpStatus.CREATED);
     }
 
 }
