29a62db3f8290c9e70a138b30995ca03ae0eee44
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-08 22:54:06 +0200

    Adds ship validation in Validator

--- backend/src/main/java/team10/app/util/Validator.java
+++ backend/src/main/java/team10/app/util/Validator.java
@@ -3,12 +3,13 @@ package team10.app.util;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import team10.app.dto.RentalEntityDto;
+import team10.app.dto.ShipDto;
 import team10.app.dto.VacationHomeDto;
 
 @Service
 @AllArgsConstructor
 public class Validator {
-
+    // Rental entity
     private static final int TITLE_MIN_LENGTH = 10;
     private static final int TITLE_MAX_LENGTH = 50;
     private static final int DESCRIPTION_MIN_LENGTH = 5;
@@ -21,10 +22,30 @@ public class Validator {
     private static final int MAX_PRICE = 10000;
     private static final int MIN_PICTURES = 5;
     private static final int MAX_PICTURES = 11;
+
+    // Vacation home
     private static final int MIN_ROOMS = 1;
     private static final int MAX_ROOMS = 20;
     private static final int MIN_BEDS = 1;
     private static final int MAX_BEDS = 100;
+
+    // Ship
+    private static final int TYPE_MIN_LENGTH = 5;
+    private static final int TYPE_MAX_LENGTH = 50;
+    private static final double MIN_LENGTH = 2;
+    private static final double MAX_LENGTH = 50;
+    private static final int MIN_ENGINE_COUNT = 1;
+    private static final int MAX_ENGINE_COUNT = 5;
+    private static final int MIN_POWER = 1;
+    private static final int MAX_POWER = 10000;
+    private static final int MIN_SPEED = 5;
+    private static final int MAX_SPEED = 200;
+    private static final int MIN_NAVIGATION_EQUIPMENT_LENGTH = 0;
+    private static final int MAX_NAVIGATION_EQUIPMENT_LENGTH = 20;
+    private static final int MIN_FISHING_EQUIPMENT_LENGTH = 0;
+    private static final int MAX_FISHING_EQUIPMENT_LENGTH = 100;
+    private static final int MIN_CAPACITY = 1;
+    private static final int MAX_CAPACITY = 100;
 
     private final AddressValidator addressValidator;
 
@@ -45,7 +66,23 @@ public class Validator {
             && inRange(MIN_BEDS, MAX_BEDS, vacationHomeDto.getBeds());
     }
 
+    public boolean validateShipDto(ShipDto shipDto) {
+        return validateRentalEntity(shipDto)
+                && inRange(TYPE_MIN_LENGTH, TYPE_MAX_LENGTH, shipDto.getType().length())
+                && inRange(MIN_LENGTH, MAX_LENGTH, shipDto.getLength())
+                && inRange(MIN_POWER, MAX_POWER, shipDto.getEnginePower())
+                && inRange(MIN_SPEED, MAX_SPEED, shipDto.getEngineCount())
+                && inRange(MIN_NAVIGATION_EQUIPMENT_LENGTH, MAX_NAVIGATION_EQUIPMENT_LENGTH, shipDto.getNavigationEquipment().size())
+                && inRange(MIN_CAPACITY, MAX_CAPACITY, shipDto.getCapacity());
+
+    }
+
     private boolean inRange(int min, int max, int num) {
         return num >= min && num <= max;
     }
+    private boolean inRange(double min, double max, double num) {
+        return num >= min && num <= max;
+    }
+
+
 }
