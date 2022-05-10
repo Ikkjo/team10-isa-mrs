e8db523ae351df7e21daf93dc54ca73d27dc5b79
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-10 23:21:42 +0200

    Fixes email sender bugs

--- backend/src/main/java/team10/app/service/EmailService.java
+++ backend/src/main/java/team10/app/service/EmailService.java
@@ -3,6 +3,7 @@ package team10.app.service;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
