a0c1ed58eb9d10f80b687bb5336957d3126ff218
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-17 13:04:54 +0200

    Refactors UserNotFoundException to use UUID

--- backend/src/main/java/team10/app/util/exceptions/UserNotFoundException.java
+++ backend/src/main/java/team10/app/util/exceptions/UserNotFoundException.java
@@ -1,8 +1,10 @@
 package team10.app.util.exceptions;
 
+import java.util.UUID;
+
 public class UserNotFoundException extends RuntimeException {
 
-    public UserNotFoundException(Long id) {
+    public UserNotFoundException(UUID id) {
         super("Could not found user " + id);
     }
 }
