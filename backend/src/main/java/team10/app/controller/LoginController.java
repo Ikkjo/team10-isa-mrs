7ab1f8ea2c15b11d6407da3d6366d8759222d430
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-18 20:48:23 +0200

    Fixes login

--- backend/src/main/java/team10/app/controller/LoginController.java
+++ backend/src/main/java/team10/app/controller/LoginController.java
@@ -1,9 +1,8 @@
 package team10.app.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,34 +14,22 @@ import team10.app.security.auth.JWTProvider;
 import team10.app.service.UserService;
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 @RequestMapping("api/v1/login")
 public class LoginController {
 
     private final UserService userService;
-
     private final JWTProvider jwtProvider;
-
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
+    private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     @PostMapping
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) throws Exception {
-
-        if (!userService.userExists(username)) {
-            throw new Exception("User dos not exist");
-        }
-
-        if (!userService.loadUserByEmail(username).get().getPassword().equals(bCryptPasswordEncoder.encode(password))){
-            throw new Exception("Invalid credentials!");
-        }
-
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(username, password);
         // is called UserDetailsService.LoadUserByUsername
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return jwtProvider.createToken(authentication);
     }
