ae046ba43252b9e939182ac4955ec5d180129990
ikkjo <ilija.kalinic@gmail.com>
2022-05-18 17:17:48 +0200

    Adds JWTAcessDeniedHandler, JWTAuthenticationEntryPoint, JWTProvider, JWTConfigurer, JWTFilter, WebMvcConfigurerAdapter

---
+++ backend/src/main/java/team10/app/security/auth/JWTAcessDeniedHandler.java
@@ -0,0 +1,16 @@
+package team10.app.security.auth;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAcessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
---
+++ backend/src/main/java/team10/app/security/config/JWTConfigurer.java
@@ -0,0 +1,20 @@
+package team10.app.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import team10.app.security.auth.JWTProvider;
+import team10.app.security.filter.JWTFilter;
+
+@RequiredArgsConstructor
+public class JWTConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private JWTProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JWTFilter customFilter = new JWTFilter(jwtProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class); }
+}
