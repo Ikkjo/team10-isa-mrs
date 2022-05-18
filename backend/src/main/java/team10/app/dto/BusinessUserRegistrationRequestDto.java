25123b55bd1db1bb804d57e78aaaaa141623abc2
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-18 20:18:03 +0200

    Fixes registration

--- backend/src/main/java/team10/app/dto/BusinessUserRegistrationRequestDto.java
+++ backend/src/main/java/team10/app/dto/BusinessUserRegistrationRequestDto.java
@@ -1,8 +1,8 @@
 package team10.app.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import team10.app.model.UserRole;
 
 @Getter
 @Setter
@@ -12,20 +12,4 @@ public class BusinessUserRegistrationRequestDto extends RegistrationRequestDto {
     private String city;
     private String country;
     private String registrationReason;
-
-    public BusinessUserRegistrationRequestDto(String firstName, String lastName, String email, String password,
-                                              UserRole role, String phoneNumber, String dateOfBirth, String address,
-                                              String city, String country, String registrationReason) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.city = city;
-        this.country = country;
-        this.registrationReason = registrationReason;
-    }
 }
