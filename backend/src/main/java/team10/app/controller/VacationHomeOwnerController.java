6b2c9c17dd43a13303550e42c96c262f733f6815
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-19 22:56:21 +0200

    Adds getAllVacationHomes to VacationHomeOwnerController

--- backend/src/main/java/team10/app/controller/VacationHomeOwnerController.java
+++ backend/src/main/java/team10/app/controller/VacationHomeOwnerController.java
@@ -3,9 +3,14 @@ package team10.app.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import team10.app.dto.VacationHomeDto;
+import team10.app.model.VacationHome;
 import team10.app.service.VacationHomeOwnerService;
+
+import java.security.Principal;
+import java.util.Set;
 
 @RestController
 @RequestMapping(path = "/api/v1/vacation-home-owner")
@@ -23,6 +28,17 @@ public class VacationHomeOwnerController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(request, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/vacation-homes")
+    public ResponseEntity<Set<VacationHomeDto>> getAllVacationHomes(Principal principal) {
+        try {
+            return new ResponseEntity<>(vacationHomeOwnerService.getAllActiveVacationHomesByOwnerEmail(principal.getName()), HttpStatus.OK);
+        }
+        catch (UsernameNotFoundException ex)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
