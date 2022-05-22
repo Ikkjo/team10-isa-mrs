b972b9a07d949175a39b3bf934f6c57d54ff704e
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-23 00:26:14 +0200

    Adds update first/last name to BusinessClientService

--- backend/src/main/java/team10/app/service/BusinessClientService.java
+++ backend/src/main/java/team10/app/service/BusinessClientService.java
@@ -5,14 +5,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import team10.app.dto.BusinessClientDto;
 import team10.app.model.BusinessClient;
-import team10.app.model.VacationHomeOwner;
 import team10.app.repository.BusinessClientRepository;
+import team10.app.repository.UserRepository;
+import team10.app.util.Validator;
+import team10.app.util.exceptions.FirstNameInvalidException;
 
 @Service
 @AllArgsConstructor
 public class BusinessClientService {
 
     private final BusinessClientRepository businessClientRepository;
+    private final UserRepository userRepository;
+    private final Validator validator;
 
     public BusinessClientDto getUserDetails(String email) throws UsernameNotFoundException {
         BusinessClient businessClient = businessClientRepository.findByEmail(email)
@@ -20,4 +24,16 @@ public class BusinessClientService {
         return new BusinessClientDto(businessClient);
     }
 
+    public void updateFirstName(String firstName, String email) throws RuntimeException {
+        if (!validator.validateFirstName(firstName))
+            throw new FirstNameInvalidException(firstName);
+        userRepository.updateFirstName(firstName, email);
+    }
+
+    public void updateLastName(String lastName, String email) {
+        if (!validator.validateLastName(lastName))
+            throw new FirstNameInvalidException(lastName);
+        userRepository.updateLastName(lastName, email);
+
+    }
 }
