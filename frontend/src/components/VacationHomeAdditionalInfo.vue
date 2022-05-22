0500f1ba2760989581e72523b74b1aa5da9d657d
Vladan Mikic <62150332+therealtgd@users.noreply.github.com>
2022-05-22 14:40:53 +0200

    Removes commented css

--- frontend/src/components/VacationHomeAdditionalInfo.vue
+++ frontend/src/components/VacationHomeAdditionalInfo.vue
@@ -20,31 +20,4 @@ export default {
 </script>
 
 <style scoped>
-/* #rental-entity-details {
-    max-width: 100%;
-}
-
-h2 {
-    margin-top: 20px;
-}
-
-.info {
-    margin-top: calc(var(--nav-height) + 40px);
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-
-.info-items > * {
-    padding: 20px 0;
-    border-bottom: 1px solid #EBEBEB;
-}
-
-.info-section {
-    width: 600px;
-    min-width: 300px;
-    max-width: 100%;
-} */
 </style>
