86f9e32124c4c9cde31de545f79932db780ec8eb
Vladan Mikic <vladanmikic2000@gmail.com>
2022-05-27 19:08:32 +0200

    Changes \n to %n in exceptions

--- backend/src/main/java/team10/app/util/exceptions/RentalEntityDescriptionInvalidException.java
+++ backend/src/main/java/team10/app/util/exceptions/RentalEntityDescriptionInvalidException.java
@@ -2,6 +2,6 @@ package team10.app.util.exceptions;
 
 public class RentalEntityDescriptionInvalidException extends RentalEntityDetailsException {
     public RentalEntityDescriptionInvalidException(String description) {
-        super(String.format("RentalEntity description invalid:\n%s", description));
+        super(String.format("RentalEntity description invalid:%n%s", description));
     }
 }
--- backend/src/main/java/team10/app/util/exceptions/RentalEntityRulesOfConductInvalidException.java
+++ backend/src/main/java/team10/app/util/exceptions/RentalEntityRulesOfConductInvalidException.java
@@ -2,6 +2,6 @@ package team10.app.util.exceptions;
 
 public class RentalEntityRulesOfConductInvalidException extends RentalEntityDetailsException {
     public RentalEntityRulesOfConductInvalidException(String rulesOfConduct) {
-        super(String.format("RentalEntity rules of conduct invalid:\n%s", rulesOfConduct));
+        super(String.format("RentalEntity rules of conduct invalid:%n%s", rulesOfConduct));
     }
 }
