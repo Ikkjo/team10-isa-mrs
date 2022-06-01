173163c9a2f52827af13324e8dd462d740f1abfb
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-01 18:12:55 +0200

    Remove unecessary imports

--- backend/src/main/java/team10/app/AppApplication.java
+++ backend/src/main/java/team10/app/AppApplication.java
@@ -1,25 +1,9 @@
 package team10.app;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.FluentQuery;
-import team10.app.model.Admin;
-import team10.app.repository.AdminRepository;
-import team10.app.service.AdminService;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.function.Function;
 
-@AllArgsConstructor
 @SpringBootApplication
 public class AppApplication {
 
