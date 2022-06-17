44412aa58effcb4f2e4faefb4b4eab9494c25df4
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-17 20:09:08 +0200

    Added forgoten file :D

--- frontend/src/views/BusinessClientReservations.vue
+++ frontend/src/views/BusinessClientReservations.vue
@@ -1,7 +1,19 @@
 <template>
 <div>
     <BusinessClientNavBar id="nav"/>
-    <div class="reservation-table-container">
+    <div class="btn-group-calendar">
+        <button class="btn calendar-view"
+            :class="{'not-selected': !calendarView}"
+            @click="calendarView=true"
+            >Calendar view
+        </button>
+        <button class="btn table-view"
+            :class="{'not-selected': calendarView}"
+            @click="calendarView=false"
+            >Table view
+        </button>
+    </div>
+    <div v-if="!calendarView" class="reservation-table-container">
         <VueGoodTable
             mode="remote"
             :pagination-options="{
@@ -32,7 +44,14 @@
             </template>
         </VueGoodTable>
     </div>
-
+    <div v-else class="calendar-wrapper">
+        <div class="calendar-container">
+            <FullCalendar
+            class='calendar'
+            :options='calendarOptions'
+            />
+        </div>
+    </div>
 </div>
 </template>
 
@@ -41,15 +60,19 @@ import BusinessClientNavBar from '@/components/BusinessClientNavBar.vue'
 import 'vue-good-table/dist/vue-good-table.css'
 import { VueGoodTable } from 'vue-good-table';
 import axios from 'axios'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 export default {
     name: 'BusinessClientReservations',
     components: {
         BusinessClientNavBar,
-        VueGoodTable
+        VueGoodTable,
+        FullCalendar
     },
     data() {
         return {
+            calendarView: true,
             isLoading: false,
             columns: [
                 {
@@ -103,6 +126,18 @@ export default {
                 perPage: 10
             },
             selectedRow: null,
+            calendarOptions: {
+                plugins: [
+                    dayGridPlugin,
+                ],
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth'
+                },
+                events: [],
+                eventClick: this.handleEventClick,
+            }
         }
     },
     methods: {
@@ -161,7 +196,21 @@ export default {
                 console.log(error);
             }) 
         },
-       
+        convertReservationToEvent(reservation) {
+            let event = {
+                id: reservation.id,
+                title: reservation.rentalEntityTitle+" reserved by "+reservation.businessClient,
+                start: new Date(reservation.startDate).toISOString().replace(/T.*$/, ''),
+                end: new Date(reservation.endDate+86400000).toISOString().replace(/T.*$/, ''),
+                color: 'orange',
+            }
+            this.calendarOptions.events.push(event);
+        },
+        handleEventClick(clickInfo) {
+            let event = clickInfo.event;
+            console.log(event.id);
+            this.$router.push({name: 'reservation-report', params: {id: event.id}});
+        }
     },
     mounted() {
         axios({
@@ -175,6 +224,7 @@ export default {
             .then((response) => {
                 this.totalRecords = response.data.totalPages
                 this.rows = response.data.reservations
+                this.rows.forEach(this.convertReservationToEvent)
             })
             .catch((error) => {
                 alert("Something went wrong. See console for output.")
@@ -184,12 +234,12 @@ export default {
 }
 </script>
 
-<style >
+<style>
 
 .reservation-table-container {
     max-width: 100%;
-    margin-top: 70px;
-    padding: 0px 10px;
+    margin-top: 15px;
+    padding: 0px 5%;
 }
 
 .filter {
@@ -218,5 +268,60 @@ export default {
     outline-color: var(--orange-primary, #f0a500);
 }
 
+.btn-group-calendar {
+    margin-top: 70px;
+    margin-left: 5%;
+}
 
+.btn-group-calendar * {
+    width: 150px;
+}
+
+.calendar-view {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.table-view {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+
+.calendar-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+.calendar-container {
+    width: 50%;
+}
+.not-selected {
+    background-color: lightgray;
+}
+
+.fc-button {
+    display: inline-block !important;
+    border: none !important;
+    border-radius: 5px !important;
+    outline: none !important;
+    color: var(--white, #fff) !important;
+    background-color: var(--orange-primary, orange) !important;
+    cursor: pointer !important;
+    font-family: inherit !important;
+    text-decoration: none !important;
+    /* transition: 0.5s !important; */
+}
+.fc-button:hover {
+    background-color: var(--orange-secondary, orange);
+}
+.fc-button:active {
+  transform: scale(0.98);
+}
+a.fc-daygrid-day-number {
+    color: black !important;
+}
+
+/* I HAVE NO IDEA WHY THIS WORKS ON CANCELING THE BORDER OF FULLCALENDAR BUTTONS BUT IT WORKS */
+.fc .fc-button-primary:not(:disabled):active, .fc .fc-button-primary:not(:disabled).fc-button-active { box-shadow: none !important; }
+.fc.fc-button-primary:before, .fc.fc-button-primary:after { box-shadow: none !important; }
+.fc .fc-button:not(:disabled) { box-shadow: none !important; }
 </style>
