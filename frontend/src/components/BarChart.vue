cbd3f6e2b46f5f2937375a6e9778f2b10c2414f9
Vladan Mikic <vladanmikic2000@gmail.com>
2022-06-19 01:06:53 +0200

    Adds Bar and Pie chart

---
+++ frontend/src/components/BarChart.vue
@@ -0,0 +1,91 @@
+<template>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs/legacy'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: {
+    Bar
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    },
+    chartOptions: {
+        type: Object,
+        default: () => ({ 
+            responsive: true,
+            maintainAspectRatio: false,
+        })
+    },
+    chartData: {
+        type: Object,
+        default: () => ({
+            labels: [
+                // for day in daily earnings
+                '17.06.2022.',
+                '18.06.2022.',
+                '19.06.2022.',
+            ],
+            datasets: [
+                {
+                    label: 'Earnings',
+                    backgroundColor: '#f0a500',
+                    data: [40, 20, 12] // for earnings in daily earnings
+                }
+            ]
+        })
+    }
+  },
+}
+</script>
--- frontend/src/views/StatisticsView.vue
+++ frontend/src/views/StatisticsView.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
-      <h1>Statistics View</h1>
+      <BusinessClientNavBar id="nav"/>
+      <div class="content">
+        <h1>Statistics View</h1>
+        <BarChart />
+        <PieChart />
+      </div>
   </div>
 </template>
 
 <script>
+import BusinessClientNavBar from '@/components/BusinessClientNavBar.vue'
+import BarChart from '@/components/BarChart.vue'
+import PieChart from '@/components/PieChart.vue'
 export default {
     name: 'StatisticsView',
+    components: {
+      BusinessClientNavBar,
+      BarChart,
+      PieChart,
+    }
 
 }
 </script>
 
 <style>
+.content {
+  margin-top: 70px;
+}
 
 </style>
