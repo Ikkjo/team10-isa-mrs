b1b62a88181cbb1e4c6e8054ed098876198f7682
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-21 13:03:43 +0200

    Add id in UserDto properties

--- backend/src/main/java/team10/app/dto/UserDto.java
+++ backend/src/main/java/team10/app/dto/UserDto.java
@@ -4,11 +4,13 @@ import lombok.*;
 import team10.app.model.User;
 import team10.app.model.UserRole;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserDto {
-
+    protected UUID id;
     protected String firstName;
     protected String lastName;
     protected String email;
@@ -17,6 +19,7 @@ public class UserDto {
     protected boolean deleted;
 
     public UserDto(User user) {
+        this.id = user.getId();
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
         this.email = user.getEmail();
