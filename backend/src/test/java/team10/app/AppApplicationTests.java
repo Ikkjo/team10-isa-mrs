2c1e1c688aa249f1e4f69aa344a2962361f13965
Vladan Mikic <vladanmikic2000@gmail.com>
2022-06-21 21:38:30 +0200

    Fixes unit tests

--- backend/src/test/java/team10/app/AppApplicationTests.java
+++ backend/src/test/java/team10/app/AppApplicationTests.java
@@ -2,11 +2,13 @@ package team10.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 
 }
--- backend/src/test/java/team10/app/util/AddressValidatorTest.java
+++ backend/src/test/java/team10/app/util/AddressValidatorTest.java
@@ -1,15 +1,11 @@
 package team10.app.util;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import team10.app.dto.AddressDto;
 import team10.app.model.Address;
 import team10.app.model.VacationHome;
@@ -21,10 +17,7 @@ import team10.app.repository.VacationHomeRepository;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.ArgumentMatchers.notNull;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
