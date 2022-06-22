ad9c19a983ce22a1f084f46b7cd0bdbdcbb4eaca
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-22 02:31:23 +0200

    Add @NoArgsConstructor to ease the pain

--- backend/src/main/java/team10/app/dto/RegistrationRequestDto.java
+++ backend/src/main/java/team10/app/dto/RegistrationRequestDto.java
@@ -2,9 +2,13 @@ package team10.app.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import team10.app.model.UserRole;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public abstract class RegistrationRequestDto {
     protected String firstName;
     protected String lastName;
