08a5cdd2663c18d932e882e96e9c872660cc52ff
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-22 04:54:02 +0200

    Hotfix: Annotate Entity to ClientReview

--- backend/src/main/java/team10/app/model/ClientReview.java
+++ backend/src/main/java/team10/app/model/ClientReview.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class ClientReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 }
